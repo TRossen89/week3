b5344ea545bd4215f824d01e519fa504ac0b4cb7
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-02-13 16:36:55 +0100

    Entity lifecycle

--- src/main/java/org/exercises/student/Main.java
+++ src/main/java/org/exercises/student/Main.java
@@ -12,27 +12,38 @@ public class Main {
 
         entityManagerFactory = HibernateConfig.getEntityManagerFactoryConfig();
 
+        // Transient state
         Student student = new Student("Tobias", "Rossen", 34, "tobiasTest@)hotmail.com");
+        // Managed and then detached state
         createStudent(student);
 
+        // Transient state
         Student student1 = new Student("Clarence", "Seedorf", 47, "seedorfTest@)hotmail.com");
+        // Managed and then detached state
         createStudent(student1);
 
+        // Transient state
         Student student2 = new Student("Nana", "Juhlin", 16, "nanaTest@)hotmail.com");
+        // Managed and then detached state
         createStudent(student2);
 
+        // Managed and then detached state
         updateStudent(1,student1);
 
+        // DB, managed, detached, transient ??
         Student studentRead1 = readStudent(1);
         Student studentRead2 = readStudent(2);
 
         System.out.println("Students read (they are supposed to be the same): \n" + studentRead1 +"\n"+studentRead2);
 
+        // Managed and then removed
         deleteStudent(1);
 
+        // DB, managed, detached, transient
         List<Student> listOfStudents = readAllStudents();
 
         System.out.println("All students: \n");
+        // Transient entities
         listOfStudents.forEach(System.out::println);
 
 
@@ -42,7 +53,10 @@ public class Main {
 
         try (var em = entityManagerFactory.createEntityManager()) {
             em.getTransaction().begin();
+            // Managed state
             em.persist(student);
+
+            // Detached state
             em.getTransaction().commit();
         }
     }
@@ -50,7 +64,10 @@ public class Main {
 
     public static Student readStudent(int id){
 
+
         try (var em = entityManagerFactory.createEntityManager()) {
+
+            // DB, managed, detached?
             return em.find(Student.class, id);
         }
     }
@@ -61,13 +78,19 @@ public class Main {
         try (var em = entityManagerFactory.createEntityManager()) {
             em.getTransaction().begin();
 
+            // DB and then managed
             var studentInDB = em.find(Student.class, id);
+
+            //Managed
             studentInDB.setFirstName(student.getFirstName());
             studentInDB.setLastName(student.getLastName());
             studentInDB.setAge(student.getAge());
             studentInDB.setEmail(student.getEmail());
 
+            // Managed, DB
             em.merge(studentInDB);
+
+            // Detached
             em.getTransaction().commit();
         }
     }
@@ -76,8 +99,13 @@ public class Main {
 
         try (var em = entityManagerFactory.createEntityManager()) {
             em.getTransaction().begin();
+            // DB and then managed
             var student = em.find(Student.class, id);
+
+            // Managed
             em.remove(student);
+
+            // Removed
             em.getTransaction().commit();
         }
     }
@@ -85,7 +113,10 @@ public class Main {
     public static List<Student> readAllStudents(){
 
         try (var em = entityManagerFactory.createEntityManager()) {
+            // DB, managed
             var query = em.createQuery("SELECT a FROM Student a", Student.class);
+
+            // Managed and detached
             return query.getResultList();
         }
     }
